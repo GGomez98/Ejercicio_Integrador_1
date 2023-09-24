e3ab54be02e5dcd62c177667c5ce7a6422b2a21d
Gaston Gomez <gaston.gomez.1998@gmail.com>
2023-09-24 14:43:37 -0300

    Se agregaron metodos a las clases Operacion y Numeracion

--- Entidades/Numeracion.cs
+++ Entidades/Numeracion.cs
@@ -13,8 +13,11 @@ namespace Entidades
     }
     public class Numeracion
     {
-        private ESistema sistema;
-        private double valorNumerico;
+        private ESistema Sistema { get; }
+        private string Valor {get; }
+        ESistema sistema;
+        double valorNumerico;
+
 
         public Numeracion(ESistema sistema, double valorNumerico)
         {
@@ -25,6 +28,40 @@ namespace Entidades
         {
             this.sistema = sistema;
             this.valorNumerico = valorNumerico;
+        }
+
+        public string ConvertirA(ESistema sistema)
+        {
+            if(sistema == ESistema.Binario)
+            {
+            }
+            else
+            {
+            }
+        }
+
+        private bool EsBinario(string cadena)
+        {
+            foreach(char caracter in cadena)
+            {
+                if(caracter != '0' && caracter != '1')
+                {
+                    return false;
+                }
+            }
+            return true;
+        }
+
+        private string DecimalBinario(int valor)
+        {
+        }
+
+        private string DecimalBinario(string valor)
+        {
+        }
+
+        private double BinarioDecimal(string valor)
+        {
         }
     }
 }
--- Entidades/Operacion.cs
+++ Entidades/Operacion.cs
@@ -2,6 +2,27 @@
 {
     public class Operacion
     {
+        private Numeracion PrimerOperando {  get; set; }
+        private Numeracion SegundoOperando { get; set; }
+        Numeracion primerOperando;
+        Numeracion segundoOperando;
+        public Operacion(Numeracion primerOperando, Numeracion segundoOperando) 
+        { 
+        }
 
+        public Numeracion Operar(char operador)
+        {
+            switch (operador)
+            {
+                case '-':
+                    break;
+                case '*':
+                    break;
+                case '/':
+                    break;
+                default:
+                    break;
+            }
+        }
     }
 }
